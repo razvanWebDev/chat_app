cc42bad90cc06e80ef2cf08cc56288a063627c94
razvanWebDev <crsn_razvan@yahoo.com>
2021-10-26 21:53:54 +0300

    display members by last msg time

--- dist/php/display_members.php
+++ dist/php/display_members.php
@@ -4,7 +4,12 @@
 <?php
  $outgoing_id = $_SESSION['unique_id'];
 
-$query = "SELECT * FROM members WHERE NOT m_unique_id={$outgoing_id}";
+$query = "SELECT * FROM members LEFT JOIN messages 
+        ON members.m_unique_id=messages.incomming_msg_id OR members.m_unique_id=messages.outgoing_msg_id 
+        WHERE NOT m_unique_id={$outgoing_id} 
+        GROUP BY members.m_unique_id 
+        ORDER BY max(messages.msg_id) DESC";
+        
 $select_members = mysqli_query($connection, $query);
 
 $output = "";
